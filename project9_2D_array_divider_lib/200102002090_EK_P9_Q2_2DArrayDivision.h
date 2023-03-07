3ba5f8867896d9cc3a9066a210194c8828f39fd7
ekoyuncal <62388510+ekoyuncal@users.noreply.github.com>
2023-03-07 14:08:04 +0300

    Delete 200102002090_EK_P9_Q2_2DArrayDivision.h

--- project9_2D_array_divider_lib/200102002090_EK_P9_Q2_2DArrayDivision.h
+++
@@ -1,9 +0,0 @@
-#ifndef _200102002090_EK_P9_Q2_2DArrayDivision
-#define _200102002090_EK_P9_Q2_2DArrayDivision
-
-void arrayPrinter(int row, int column,float array[row][column]);
-
-void arrayDivider(float *arrayPtr,int row,int column);
-
-#endif
-

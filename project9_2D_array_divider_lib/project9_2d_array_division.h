a011158afe867983370e088ad7bad392304fc5c7
ekoyuncal <62388510+ekoyuncal@users.noreply.github.com>
2023-03-07 14:08:46 +0300

    My C Homeworks

---
+++ project9_2D_array_divider_lib/project9_2d_array_division.h
@@ -0,0 +1,9 @@
+#ifndef _project9_2d_array_division
+#define _project9_2d_array_division
+
+void arrayPrinter(int row, int column,float array[row][column]);
+
+void arrayDivider(float *arrayPtr,int row,int column);
+
+#endif
+
